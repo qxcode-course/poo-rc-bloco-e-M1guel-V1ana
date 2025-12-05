b394a976617bffff9576ebeec19589b9f60b498c
Miguel <miguel@gmail.com>
2025-12-05 01:17:36 -0300

    implementando classes

--- tasks/poo/zoo/py/draft.py
+++ tasks/poo/zoo/py/draft.py
@@ -7,6 +7,9 @@ class Animal(ABC):
     def __init__(self, nome: str):
         self.__nome = nome
 
+    def getNome(self):
+        return self.__nome
+
     @abstractmethod
     #def apresetar_nome(self):
     #   pass  #  metodo sem orientação
@@ -19,12 +22,23 @@ class Animal(ABC):
 
 
 class Zebra(Animal):
+    def apresentar_nome(self):
+        print(f"Eu sou um(a) <{self.getNome()}>!")
 
 
 
+class Lobo(Animal):
+    def apresentar_nome(self):
+        print(f"Eu sou um(a) <{self.getNome()}>!")
 
 
-#animais: list[any] = []
 
-#for animais in Animal:
-#   ...
+class Pato(Animal):
+    def apresentar_nome(self):
+        print(f"Eu sou um(a) <{self.getNome()}>")
+
+
+animais: list[any] = [Zebra("Mico"), Lobo("Alfred"), Pato("Geraldinho")]
+
+for animal in animais:
+    animal.apresentar_nome()
