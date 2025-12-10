d5ce86d36204540ed5eac20b4656d49a0c2a8769
Miguel <miguel@gmail.com>
2025-12-10 11:23:23 -0300

    eu e o cadu fizemos o começo do código e um pouco das classes

--- tasks/poo/estacionamento/py/draft.py
+++ tasks/poo/estacionamento/py/draft.py
@@ -1,17 +1,50 @@
 from abc import ABC, abstractmethod
 
-
 class Veiculo(ABC):
-    def __init__(self, id: str, tipo: str, hora_entrada: int ):
+    def __init__(self, id: str, tipo: str, horaentrada: float):
         self.id = id
         self.tipo = tipo
-        self.entrada = hora_entrada
+        self.horaentrada = horaentrada
 
-
-
-
-    def setEntrada(self, entrada: int):
-        self.entrada = entrada
+    def setEntrada(self, hora: int):
+        self.horaentrada = hora
 
     def getEntrada(self):
-        return self.entrada
+        return self.horaentrada
+    def getTipo(self):
+        return self.tipo
+    def getId(self):
+        return self.id
+
+    def __str__(self):
+        return f"{self.tipo}: {self.id}: {self.horaentrada}}"
+
+
+    @abstractmethod
+    def calcHorasaida(self, horasaida: int):
+        pass
+
+
+
+class Bike(Veiculo):
+    def __init__(self, id, tipo, horaentrada):
+        super().__init__(id, tipo, horaentrada)
+
+    def calcHorasaida(self, horasaida: float) -> float:
+        return 3.00
+
+class Moto(Veiculo):
+    def __init__(self, id, tipo, horaentrada):
+        super.__init__(id, tipo, horaentrada)
+
+    def calcHorasaida(self, horasaida: int) -> float:
+        valor = self.horaentrada / 20
+        return valor
+
+
+class Carro(Veiculo):
+    def __init__(self, id, tipo, horaentrada):
+        super().__init__(id, tipo, horaentrada)
+
+    def calcHorasaida(self, horasaida: int) -> float:
+        valor =
