e993708b0e3181629c62213128b58c5bf3c39345
Miguel <miguel@gmail.com>
2025-12-14 15:46:51 -0300

    criando a classe estacionamento

--- tasks/poo/estacionamento/py/draft.py
+++ tasks/poo/estacionamento/py/draft.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Veiculo(ABC):
-    def __init__(self, id: str, tipo: str, horaEntrada: int):
+    def __init__(self, id: str, tipo: str):
         self.id = id
         self.tipo = tipo
-        self.horaEntrada = horaEntrada
+        self.horaEntrada = None
 
     def getEntrada(self) -> int:
         return self.horaEntrada
@@ -14,35 +14,51 @@ class Veiculo(ABC):
     def getId(self) -> str:
         return self.id
 
-    def sethoraEntrada(self, hora: int) -> None:
+    def setEntrada(self, hora: int) -> None:
         self.horaEntrada = hora
 
     @abstractmethod
-    def calcularHorasaida(self, horaSaida: int):
+    def calcularValor(self, horaSaida: int):
         pass
 
     def __str__(self):
-        return f"{self.id}: {self.tipo} : {self.horaEntrada}"
-
-
-
+        return f"{self.tipo}: {self.id} : {self.horaEntrada}"
 
 
 class Bike(Veiculo):
-    def __init__(self):
+    def __init__(self, id: str):
+        super().__init__(id, "Bike")
+    def calcularValor(self, horaSaida: int) -> float:
+        return 3.0
 
 
+class Moto(Veiculo):
+    def __init__(self, id: str):
+        super().__init__(id, "Moto")
+
+    def calcularValor(self, horaSaida: int) -> float:
+        tempo = horaSaida - self.getEntrada()
+        return tempo / 20
+
+class Carro(Veiculo):
+    def __init__(self, id: str):
+        super.__init__(id, "Carro")
+
+    def calcularValor(self, horaSaida: int) -> float:
+        tempo = horaSaida - self.getEntrada()
+        valor = tempo / 10
+        return max(valor, 5.0)
 
 
+class Estacionamento:
+    def __init__(self, horaAtual: int):
+        self.horaAtual = horaAtual
+        self.veiculos: list[Veiculo] = []
 
-
-
-
-
+    def procurarVeiculo
 
 def main():
     estacionar: list[Estacionamento] = []
-
 
     while True:
 
